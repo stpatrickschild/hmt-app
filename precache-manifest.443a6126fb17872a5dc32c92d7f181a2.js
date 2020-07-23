c57af31eff8102fcaa0743403d993acfa5cd8fc4
Hannah Tekié <hannahtekie@gmail.com>
2020-07-22 23:24:51 -0700

    Updates

---
+++ precache-manifest.443a6126fb17872a5dc32c92d7f181a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f81240e4797b00d73bc08881679948",
+    "url": "/hmt-app/index.html"
+  },
+  {
+    "revision": "2ecc898441ec83a0b119",
+    "url": "/hmt-app/static/css/main.1ed9e829.chunk.css"
+  },
+  {
+    "revision": "f427668f954f7ba42066",
+    "url": "/hmt-app/static/js/2.4d3d6b0f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/hmt-app/static/js/2.4d3d6b0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ecc898441ec83a0b119",
+    "url": "/hmt-app/static/js/main.868a4ad2.chunk.js"
+  },
+  {
+    "revision": "5ca8424c1e3799a30e87",
+    "url": "/hmt-app/static/js/runtime-main.7fdfd0e1.js"
+  },
+  {
+    "revision": "10535061288587b35bfe9325a7df5f6e",
+    "url": "/hmt-app/static/media/happypplImage.10535061.jpg"
+  }
+]);
